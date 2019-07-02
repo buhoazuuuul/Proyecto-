48dc97b2a50135bf924e59f4f8e0eafb2dbbe78d
Jose <jose.echeverri@upb.edu.co>
2019-07-01 20:51:51 -0500

    Nueva coneccion con la bd

--- php/conexion.php
+++ php/conexion.php
@@ -1,16 +1,36 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "innexu";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "innexu";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<script>alert('Se hizo la conexion')</script>";
-    }
-catch(PDOException $e)
+// try {
+//     $conn = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "Connection Success";
+// } catch (PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage();
+// }
+
+class Connection
+{
+    private  $server = "mysql:dbname=innexu;host=127.0.0.1";
+    private  $user = "root";
+    private  $pass = "";
+    private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+    protected $con;
+
+    public function openConnection()
     {
-    echo "Connection failed: " . $e->getMessage();
+        try {
+            $this->con = new PDO($this->server, $this->user, $this->pass, $this->options);
+            return $this->con;
+        } catch (PDOException $e) {
+            echo "There is some problem in connection: " . $e->getMessage();
+        }
     }
-?>
+    public function closeConnection()
+    {
+        $this->con = null;
+    }
+}
--- php/updateUser.php
+++ php/updateUser.php
@@ -1,12 +1,28 @@
 <?php
-require_once("DbPDO.class.php");
+include_once('conexion.php');
+
+try {
+    $database = new Connection();
+    $db = $database->openConnection();
+    $doc = intval($_POST['num_doc']);
+    $tel = intval($_POST['telefono']);
+    $sql = "UPDATE `usuario` SET `num_doc` = :num_doc , `tipo_doc`=:tipo_doc, `nombre` = :nombre,`apellido`=:apellido,`telefono` =:telefono,`email`=:email, `residencia` = :residencia, `fecha`=:fecha,`usuario`=:usuario WHERE `num_doc` = :num_doc";
+    $sth = $db->prepare($sql);
+    $affectedrows  = $db->exec($sql);
+    if (isset($affectedrows)) {
+        echo "Record has been successfully updated";
+    }
+} catch (PDOException $e) {
+    echo "There is some problem in connection: " . $e->getMessage();
+}
+
 
 // Instancia de la clase DbPDO
-$mipdo = new DbPDO();
-$doc = intval($_POST['num_doc']);
-$tel = intval($_POST['telefono']);
+// $mipdo = new DbPDO();
+// $doc = intval($_POST['num_doc']);
+// $tel = intval($_POST['telefono']);
 
 // $updateUser = $mipdo->query("UPDATE `usuario` SET `num_doc` = :num_doc , `tipo_doc`=:tipo_doc, `nombre` = :nombre,`apellido`=:apellido,`telefono` =:telefono,`email=:email, `residencia` = :residencia, `fecha`=:fecha,`usuario`=:usuario WHERE `num_doc` = :num_doc");
-$updateUser = $mipdo->query("UPDATE usuario SET nombre = 'Jose Manuel' WHERE num_doc = 12345678");
+// $updateUser = $mipdo->query("UPDATE usuario SET apellido = 'Rosas' WHERE num_doc = 12345678");
 
-echo $updateUser;
+// echo $updateUser;
