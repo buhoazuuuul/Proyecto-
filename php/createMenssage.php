270a96d4f62774af75bf98f199a62dfc2debca24
Karyme <karymecardona012@gmail.com>
2019-07-03 23:41:48 -0500

    Avance para insertar mensaje

--- php/createMenssage.php
+++ php/createMenssage.php
@@ -7,8 +7,8 @@ $mipdo = new DbPDO();
 //Encriptando la contrasenna
 $mipdosword = password_hash($_POST['pass'], PASSWORD_BCRYPT);
 // Haciendo el binding mediante el método bindMas
-$mipdo->bindMas(array("num_doc" => $_POST['categoria'], "tipo_doc" => $_POST['tipo_doc'], "nombre" => $_POST['nombre'], "apellido" => $_POST['apellido'], "dependencia" => $_POST['dependencia'], "telefono" => $_POST['telefono'], "email" => $_POST['email'], "usuario" => $_POST['usuario'], "pass" => $mipdosword));
+$mipdo->bindMas(array("id_mensaje" => $_POST['id_mensaje'], "categoria" => $_POST['categoria'], "asunto" => $_POST['asunto'], "lugar" => $_POST['lugar'], "fecha_hora" => $_POST['fecha_hora'], "prioridad" => $_POST['prioridad'], "confidencialidad" => $_POST['confidencialidad'], "texto" => $_POST['texto'], "adjunto" => $_POST['adjunto']));
 
-$datos = $mipdo->query("INSERT INTO secretario (num_doc,tipo_doc,nombre,apellido,dependencia,telefono,email,usuario,pass) VALUES (:num_doc,:tipo_doc,:nombre,:apellido,:dependencia,:telefono,:email,:usuario,:pass)");
+$datos = $mipdo->query("INSERT INTO mensaje (id_mensaje,categoria,asunto,lugar,fecha_hora,prioridad,confidencialidad,texto,adjunto) VALUES (:id_mensaje,:categoria,:asunto,:lugar,:fecha_hora,:prioridad,:confidencialidad,:texto,:adjunto) ");
 
 echo $datos;
