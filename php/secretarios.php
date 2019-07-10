2d78b5c180b338ec71cd5a932019dafe30aa5c6e
Karyme <karymecardona012@gmail.com>
2019-07-09 20:03:02 -0500

    Inserta secretarios en la BD

--- php/secretarios.php
+++ php/secretarios.php
@@ -1,14 +1,15 @@
-<?php
-require_once("DbPDO.class.php");
+
+<?php//require_once("DbPDO.class.php");
 
 // Instancia de la clase DbPDO
-$mipdo = new DbPDO();
+//$mipdo = new DbPDO();
 
 //Encriptando la contrasenna
-$mipdosword = password_hash($_POST['pass'], PASSWORD_BCRYPT);
+//$mipdosword = password_hash($_POST['pass'], PASSWORD_BCRYPT);
 // Haciendo el binding mediante el método bindMas
-$mipdo->bindMas(array("num_doc" => $_POST['num_doc'], "tipo_doc" => $_POST['tipo_doc'], "nombre" => $_POST['nombre'], "apellido" => $_POST['apellido'], "dependencia" => $_POST['dependencia'], "telefono" => $_POST['telefono'], "email" => $_POST['email'], "usuario" => $_POST['usuario'], "pass" => $mipdosword));
+//$mipdo->bindMas(array("num_doc" => $_POST['num_doc'], "tipo_doc" => $_POST['tipo_doc'], "nombre" => $_POST['nombre'], "apellido" => $_POST['apellido'], "dependencia" => $_POST['dependencia'], "telefono" => $_POST['telefono'], "email" => $_POST['email'], "usuario" => $_POST['usuario'], "pass" => $mipdosword));
 
-$datos = $mipdo->query("INSERT INTO secretario (num_doc,tipo_doc,nombre,apellido,dependencia,telefono,email,usuario,pass) VALUES (:num_doc,:tipo_doc,:nombre,:apellido,:dependencia,:telefono,:email,:usuario,:pass)");
+//$datos = $mipdo->query("INSERT INTO secretario (num_doc,tipo_doc,nombre,apellido,dependencia,telefono,email,usuario,pass) 
+//VALUES (:num_doc,:tipo_doc,:nombre,:apellido,:dependencia,:telefono,:email,:usuario,:pass)");
 
-echo $datos;
+//echo $datos;
