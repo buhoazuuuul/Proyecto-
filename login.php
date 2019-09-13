f34696d89ead8045be70f4ad9aa3666832efaff4
Karyme <karymecardona012@gmail.com>
2019-09-12 22:29:14 -0500

    Colores

--- login.php
+++ login.php
@@ -10,8 +10,8 @@
   <title>Iniciar sesión</title>
 
   <!-- Favicons -->
-  <link href="img/favicon.png" rel="icon">
-  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="img/INNEXU1.png" rel="icon">
+  <link href="img/INNEXU1.png" rel="apple-touch-icon">
 
   <!-- Bootstrap core CSS -->
   <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
