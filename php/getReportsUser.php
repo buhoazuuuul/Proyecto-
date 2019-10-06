0253a568cf13fe7b797d216d4708160797e8df9a
Jose Manuel <you@example.com>
2019-10-06 16:44:40 -0500

    url reportes funcionando

--- php/getReportsUser.php
+++ php/getReportsUser.php
@@ -8,9 +8,7 @@
   define('DBPassword','');
   require(__DIR__ . "/PDO-Class/PDO.class.php");
   $DB = new Db(DBHost, DBPort, DBName, DBUser, DBPassword);
-  date_default_timezone_set('America/Bogota');
-
-  //Inner join tables
+  //Inner join tabsles
   $data = $DB->query("SELECT reporte.id, reporte.prioridad, reporte.asunto,reporte.fecha_hora, secretario.nombre , reporte.adjunto FROM reporte INNER JOIN reportes_x_respuestas ON reporte.id = reportes_x_respuestas.reporte_id 
                                     INNER JOIN secretario ON reportes_x_respuestas.secretario_id = secretario.id
                                     WHERE reportes_x_respuestas.usuario_id = ? ORDER BY reporte.fecha_hora DESC", array( $_POST['id_usuario']));
